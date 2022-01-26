2e15755bd681e500cb734908fef10a160646536a
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-25 22:57:30 -0800

    boiler template

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS museum_db;
+DROP DATABASE IF EXISTS sub_calendar;
 
-CREATE DATABASE museum_db;
+CREATE DATABASE sub_calendar;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
+const User = require('./User');
+const Gallery = require('./Gallery');
+const Painting = require('./Painting');
 
-module.exports = {  };
+Gallery.hasMany(Painting, {
+  foreignKey: 'gallery_id',
+});
+
+Painting.belongsTo(Gallery, {
+  foreignKey: 'gallery_id',
+});
+
+module.exports = { User, Gallery, Painting };
