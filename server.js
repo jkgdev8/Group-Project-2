a8339798a8889a0f59234cedbf05371359be838e
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-27 20:44:29 -0800

    final broiler

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -7,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -22,7 +21,9 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -33,6 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
