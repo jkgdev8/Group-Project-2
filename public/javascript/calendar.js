8119873ad7d5c3f4d87e620b4b68f9fc46aaa3f4
Jkgdev8 <juliennegarcia@Juliennes-MacBook-Pro.local>
2022-02-10 16:08:01 -0800

    80% done

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,9 +1,35 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    height: 'auto',
+    // stickyHeaderDates: false, // for disabling
+
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'listMonth,listYear'
+    },
+
+    // customize the button names,
+    // otherwise they'd all just say "list"
+    views: {
+      listMonth: { buttonText: 'list month' },
+      listYear: { buttonText: 'list year' }
+    },
+
+    initialView: 'listMonth',
+    initialDate: '2022-01-01',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    events: [
+      {
+        title: 'WIP',
+        daysOfWeek: [ 1, 2, 3 ],
+        duration: '00:30'
+      }
+    ]
+  });
+
+  calendar.render();
 });
- 
